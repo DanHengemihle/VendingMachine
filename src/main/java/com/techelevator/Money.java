9d6b6f90fac0d84deb5c2f8b5dd1f4e302e69541
William Kendig <swimmaay@gmail.com>
2022-07-29 11:02:02 -0400

    money

--- src/main/java/com/techelevator/Money.java
+++ src/main/java/com/techelevator/Money.java
@@ -1,4 +1,11 @@
 package com.techelevator;
 
+import java.math.BigDecimal;
+
 public class Money {
+    private BigDecimal dollar = new BigDecimal("1.00");
+    private BigDecimal quarter = new BigDecimal("0.25");
+    private BigDecimal dime = new BigDecimal("0.10");
+    private BigDecimal nickel = new BigDecimal("0.05");
+
 }
